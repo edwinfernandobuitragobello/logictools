e82f67db6659ab17427821489170f2f78b1c2262
Jonathan Velandia <jjyyvvff@gmail.com>
2017-08-10 18:27:59 -0500

    Cambio 1

--- resources/views/index-v1.blade.php
+++ resources/views/index-v1.blade.php
@@ -46,22 +46,7 @@
     <!-- HEADER -->
     <header id="pageTop" class="header-wrapper">
       <!-- COLOR BAR -->
-      <div class="container-fluid color-bar top-fixed clearfix">
-        <div class="row">
-          <div class="col-sm-1 col-xs-2 bg-color-1">fix bar</div>
-          <div class="col-sm-1 col-xs-2 bg-color-2">fix bar</div>
-          <div class="col-sm-1 col-xs-2 bg-color-3">fix bar</div>
-          <div class="col-sm-1 col-xs-2 bg-color-4">fix bar</div>
-          <div class="col-sm-1 col-xs-2 bg-color-5">fix bar</div>
-          <div class="col-sm-1 col-xs-2 bg-color-6">fix bar</div>
-          <div class="col-sm-1 bg-color-1 hidden-xs">fix bar</div>
-          <div class="col-sm-1 bg-color-2 hidden-xs">fix bar</div>
-          <div class="col-sm-1 bg-color-3 hidden-xs">fix bar</div>
-          <div class="col-sm-1 bg-color-4 hidden-xs">fix bar</div>
-          <div class="col-sm-1 bg-color-5 hidden-xs">fix bar</div>
-          <div class="col-sm-1 bg-color-6 hidden-xs">fix bar</div>
-        </div>
-      </div>
+     
 
       <!-- TOP INFO BAR -->
       <div class="top-info-bar bg-color-7 hidden-xs">

69ed3962ab633d4e3c1164c4a7ea217c32e24f80
Jonathan Velandia <jjyyvvff@gmail.com>
2017-08-14 08:24:30 -0500

    Ajuste index

--- resources/views/index-v1.blade.php
+++ resources/views/index-v1.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<<<<<<< HEAD
+<head>
   <!-- SITE TITTLE -->
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -39,49 +39,6 @@
 
 </head>
 
-<body class="body-wrapper">
-
-
-  <div class="main-wrapper">
-    <!-- HEADER -->
-    <header id="pageTop" class="header-wrapper">
-      <!-- COLOR BAR -->
-     
-=======
-  <head>
-      <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Logictools</title>
-    <!-- PLUGINS CSS STYLE -->
-    <link href="plugins/jquery-ui/jquery-ui.css" rel="stylesheet">
-      <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="plugins/rs-plugin/css/settings.css" media="screen">
-    <link rel="stylesheet" type="text/css" href="plugins/selectbox/select_option1.css">
-    <link rel="stylesheet" type="text/css" href="plugins/owl-carousel/owl.carousel.css" media="screen">
-    <link rel="stylesheet" type="text/css" href="plugins/isotope/jquery.fancybox.css">
-    <link rel="stylesheet" type="text/css" href="plugins/isotope/isotope.css">
-      
-    <!-- GOOGLE FONT -->
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Dosis:400,300,600,700' rel='stylesheet' type='text/css'>
-
-    <!-- CUSTOM CSS -->
-    <link href="css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/colors/default.css" id="option_color">
-
-    <!-- Icons -->
-    <link rel="shortcut icon" href="images/favicon.png">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
   <body class="body-wrapper">
       <div class="main-wrapper">
         <header id="pageTop" class="header-wrapper">
@@ -116,7 +73,6 @@
                       <img src="images/logo.png" alt="Logictools" style="width: 70%">
                     </a>
                   </div>
->>>>>>> 367be8ce17dce80014eb546ceda03d8efb528b97
 
                   <div class="collapse navbar-collapse navbar-ex1-collapse">
                     <ul class="nav navbar-nav navbar-right">
